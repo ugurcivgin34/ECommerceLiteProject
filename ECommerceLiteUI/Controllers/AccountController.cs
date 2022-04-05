7ebb02ce1927ee31766942e9d66cf39de71e233a
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-04-05 15:00:28 +0300

    Account/Register işlemi bitti.

---
+++ ECommerceLiteUI/Controllers/AccountController.cs
@@ -0,0 +1,135 @@
+﻿using ECommerceLiteBLL.Account;
+using ECommerceLiteBLL.Repository;
+using ECommerceLiteBLL.Settings;
+using ECommerceLiteEntity.Enums;
+using ECommerceLiteEntity.IdentityModels;
+using ECommerceLiteEntity.Models;
+using ECommerceLiteEntity.ViewModels;
+using ECommerceLiteUI.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceLiteUI.Controllers
+{
+    public class AccountController : BaseController
+    {
+        //Global alan
+        //Not: Bir sonraki projede repoları UI'ın içinde NEW'lemeyeceğiz!
+        //Çünkü bu bağımlılık oluşturur! Bir sonraki projede bağımlılıkları tersine çevirme işlemi olarak bilinen Dependecny Injection işlemleri
+        //yapacağız.
+
+        CustomerRepo myCustomerRepo = new CustomerRepo();
+        PassiveUserRepo myPassiveUserRepo = new PassiveUserRepo();
+        UserManager<ApplicationUser> myUserManager = MembershipTools.NewUserManager();
+        UserStore<ApplicationUser> myUserStore = MembershipTools.NewUserStore();
+        RoleManager<ApplicationRole> myRoleManager = MembershipTools.NewRoleManager();
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            //KAyıt ol sayfası
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] //Güvenliği sağlama kamacıyla bot hesaplardan bu metoda erişlemzsin diye kullandık
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid) // Model validasyonları sağladı mı
+                {
+                    return View(model);
+                }
+                var checkUserTC = myUserStore.Context.Set<Customer>()
+                    .FirstOrDefault(x => x.TCNumber == model.TCNumber)?.TCNumber;
+                if (checkUserTC != null) // Buldu 
+                {
+                    ModelState.AddModelError("", "Bu TC numarası ile daha önceden sisteme kayıt yapılmıştır!");
+                    return View(model);
+                }
+                //To Do : Soru işaretli silinip debuglanacak
+                var checkUserEmail = myUserStore.Context.Set<ApplicationUser>()
+                    .FirstOrDefault(x => x.Email == model.Email)?.Email;
+                if (checkUserEmail != null)
+                {
+                    ModelState.AddModelError("", "Bu email ile daha önceden sisteme kayıt yapılmıştır!");
+                    return View(model);
+                }
+
+                //Artık sisteme kayıt olabilir...
+                var newUser = new ApplicationUser()
+                {
+                    Name = model.Name,
+                    Surname = model.Surname,
+                    Email = model.Email,
+                    UserName = model.TCNumber
+                };
+
+                //aktivasyon kodu üretelim
+                var activationCode = Guid.NewGuid().ToString().Replace("-", "");
+
+                //Artık ekleyelim
+                var createResult = myUserManager.CreateAsync(newUser, model.Password);
+
+                //To Do: createResult.Isfault ne acaba ? debuglarken bakalım
+
+                if (createResult.Result.Succeeded)
+                {
+                    //göre başarıyla tamamlandıysa kişi aspnetusers tablosunna eklenmiştir
+                    //Yeni kayıt olduğu için bu kişiye pasif rol verilecektir
+                    //Kişi emailiine gelen aktivasyon koduna tıklarsaa pasifledikten çıkıp  customer olabilir.
+
+                    await myUserManager.AddToRoleAsync(newUser.Id, Roles.Passive.ToString());
+                    PassiveUser myPassiveUser = new PassiveUser()
+                    {
+                        UserId = newUser.Id,
+                        TCNumber = model.TCNumber,
+                        IsDeleted = false,
+                        LastActiveTime = DateTime.Now
+                    };
+
+                    //myPassiveUsrRepo.Insert(myPassiveUser);
+                    await myPassiveUserRepo.InsertAsync(myPassiveUser);
+
+                    //email gönderilecek
+                    // site adresini alıyoruz.
+                    var siteURL = Request.Url.Scheme + Uri.SchemeDelimiter
+                        + Request.Url.Host +
+                        (Request.Url.IsDefaultPort ? "" : ":" + Request.Url.Port);
+                    await SiteSettings.SendMail(new MailModel()
+                    {
+                        To = newUser.Email,
+                        Subject = "ECommerceLite Site Aktivasyon Emaili",
+                        Message = $"Merhaba {newUser.Name} {newUser.Surname}," +
+                        $"<br/>Hesabınızı aktifleştirmek için <b>" +
+                        $"<a href='{siteURL}/Account/Activation?" +
+                        $"code={activationCode}'>Aktivasyon Linkine</a></b> tıklayınız..."
+                    });
+                    // işlemler bitti...
+                    return RedirectToAction("Login", "Account", new { email = $"{newUser.Email}" });
+
+
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Kayıt işleminde beklenmedik bir hata oluştu");
+                    return View(model);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //yarın yazalım
+
+            }
+        }
+
+    }
+}
---
+++ ECommerceLiteUI/Models/RegisterViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerceLiteUI.Models
+{
+    public class RegisterViewModel
+    {
+        //Kayıt Modeli içinde siteye kayıt olmak isteyen kişilerden hangi bilgileri alacağımızı belirleyeceğiz
+        //TcKimlik,isim soy isim email varsa (eğer yazdıysak telefon,cinsiyet vb) alanlarını tanınlayalım
+
+        //Not: Data Annotation'ları kullanarak validation kurallarını belirlediğimiz için kapsüllemeye gerek kalmadı.
+
+        [Required]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "Tc kimlik numarası 11 haneli olmalıdır")]
+        [Display(Name = "Tc Kimlik")]
+        public string TCNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Ad")]
+        [StringLength(maximumLength: 30, MinimumLength = 2, ErrorMessage = "İsminizin uzunluğu 2 ile 30 karakter aralığında olmalıdır.")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Soyad")]
+        [StringLength(maximumLength: 30, MinimumLength = 2, ErrorMessage = "Soyadınızın uzunluğu 2 ile 30 karakter aralığında olmalıdır.")]
+        public string Surname { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Şifre")]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^[a-zA-Z]\w{4,14}$", ErrorMessage = @"	
+            Şifrenin ilk karakteri bir harf olmalı, en az 5 en fazla 15 karakter içermeli ve harflerden başka karakter içermemelidir,
+            sayılar ve alt çizgi kullanılabilir")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Şifre Tekrarı")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="Şifreler uyuşmuyor.Tekrar deneyiniz")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
