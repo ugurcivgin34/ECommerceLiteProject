7e741776983c45daa555e7c8800d04804f4265f4
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-04-08 15:01:22 +0300

    Login tasarlandı.

--- ECommerceLiteUI/Global.asax.cs
+++ ECommerceLiteUI/Global.asax.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ECommerceLiteBLL.Account;
+using ECommerceLiteEntity.Enums;
+using ECommerceLiteEntity.IdentityModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +20,51 @@ namespace ECommerceLiteUI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //NOT : Application_Start :
+            //Uygulama ilk kez çalıştığında bir defaya mahsus olmak üzere çalışır.
+            //Bu nedenle ben uyg. İlk kez çalıştığında db'de Roller ekli mi diye bakmak istiyorum.
+            //Ekli değilse rolleri Enum'dan çağırıp ekleyelim
+            //Ekli ise bir şey yapmaya gerek kalmıyor.
+
+            //Adım 1: Rollere bakacağım --> Role Manager
+            var myRoleManager = MembershipTools.NewRoleManager();
+            //Adım 2: Rollerin isimlerini almak (ipucu --> Enum)
+            var allRoles = Enum.GetNames(typeof(Roles));//İ.eride kaç enum var ise string dizi olarak getirecek bana
+            //Adım 3: Bize gelen diziyi tek tek tek döneceğiz (döngü)
+            foreach (var item in allRoles)
+            {
+                //Adım 4: Acaba bu rol DB'de ekli mi?
+                if (!myRoleManager.RoleExists(item))//eğer rol yoksa ekli değilse
+                {
+                    //Adım 5: Rolü ekle!
+                    //------------1. YOL-------------
+                    //ApplicationRole role = new ApplicationRole()
+                    //{
+                    //    Name = item
+                    //};
+                    //myRoleManager.Create(role);
+                    //------------2. YOL-------------
+                    myRoleManager.Create(new ApplicationRole()
+                    {
+                        Name = item
+                    });
+                }
+            }
+
+        }
+        protected void Application_Error()
+        {
+            //NOT : İhtiyacım olursa internetten Global.asax'ın metotlarına bakıp kullanabilirim...
+
+            //Örneğin: Application_Error : Uygulama içinde istenmeyen bir hata meydana geldiğinde çalışır.
+            //Bu metodu yazarsak o hatayı loglayıpsorunu çözebiliriz. 
+
+            Exception ex = Server.GetLastError();
+
+            //ex loglanacak
+
         }
     }
 }
+
