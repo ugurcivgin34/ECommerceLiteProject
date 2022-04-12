afdaa9a780d73e24ad395ff7060c0d571ccd5d7c
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-04-12 14:16:55 +0300

    303 logosu eklendi.ProductController eklendi.Crud işlemlerine başlayacağız

---
+++ ECommerceLiteUI/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceLiteUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        //Bu controllera Admin gibi yetkili kişiler erişecektir.
+        //Burada ürünlerin listelenmesi,ekleme,silme,güncelleme işlemleri yapılacaktır.
+        public ActionResult ProductList()
+        {
+            return View();
+        }
+    }
+}
