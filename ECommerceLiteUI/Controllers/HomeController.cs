a47774f1bb64558e8afc2ad74f8c9fb699dc31ac
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-04-19 15:03:05 +0300

    Vitrinde ürün sergilemek için Home Index giydirilmeye başlandı.

--- ECommerceLiteUI/Controllers/HomeController.cs
+++ ECommerceLiteUI/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ECommerceLiteBLL.Repository;
+using ECommerceLiteUI.Models;
+using Mapster;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,22 +11,48 @@ namespace ECommerceLiteUI.Controllers
 {
     public class HomeController : Controller
     {
+        CategoryRepo myCategoryRepo = new CategoryRepo();
+        ProductRepo myProductRepo = new ProductRepo();
         public ActionResult Index()
         {
-            return View();
+            //Ana kategorileriden 4 tanesini viewbag ile sayfaya gönderelim
+            var categoryList = myCategoryRepo.AsQueryable().Where(x => x.BaseCategoryId == null).Take(4).ToList();
+
+            ViewBag.CategoryList = categoryList.OrderByDescending(x => x.Id);
+
+            //Ürünler
+            var productList = myProductRepo.AsQueryable().Where(x => x.IsDeleted == false && x.Quantity >= 1).Take(10).ToList();
+            List<ProductViewModel> model = new List<ProductViewModel>();
+            foreach (var item in productList)
+            {
+                //mapster : S
+                //model.Add(item.Adapt<ProductViewModel>());
+                var product = new ProductViewModel()
+                {
+                    Id = item.Id,
+                    CategoryId = item.CategoryId,
+                    ProductName = item.Description,
+                    Quantity = item.Quantity,
+                    Discount = item.Discount,
+                    RegisterDate = item.RegisterDate,
+                    Price = item.Price,
+                    ProductCode = item.ProductCode
+                    //isDeleted alanını viewmodelin içine eklemeyi unuttuk.Çünkü isDeleted alanını daha dün ekledik . Viewmodeli geçen hafta oluşturduk.
+                };
+                product.GetCategory();
+                product.GetProductPictures();
+                model.Add(product);
+            }
+
+            return View(model);
         }
+
+
+
+        
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
